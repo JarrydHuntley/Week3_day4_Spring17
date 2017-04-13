a7e1533595650816ffcc29564082d5d911afffe7
Jarryd Huntley <Jarryd Huntley>
2017-04-13 11:30:58 -0400

    Added in a test case for the automobile class.

---
+++ Week3_day4-Spring17/Automobile.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week3_day4_Spring17
+{
+    class Automobile : Vehicle
+    {
+        private int numberOfWheels;
+        private float wheelSize;
+        private int numberOfDoors;
+
+        public int NumberOfWheels
+        {
+            get { return this.numberOfWheels;}
+        }
+
+        public Automobile( int numberOfWheels, float wheelSize, int numberOfDoors, int seats, int carryingCapacity, string color, double movementSpeed)
+        {
+            this.numberOfWheels = numberOfWheels;
+            this.wheelSize = wheelSize;
+            this.numberOfDoors = numberOfDoors;
+            this.seats = seats;
+            this.carryingCapacity = carryingCapacity;
+            this.color = color;
+            this.movementSpeed = movementSpeed;
+        }
+
+
+    }
+}
---
+++ Week3_day4-Spring17/Boat.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week3_day4_Spring17
+{
+    class Boat : Vehicle
+    {
+        private double waterDrag;
+
+        public Boat(int seats, int carryingCapacity, string color, double movementSpeed, double waterDrag)
+        {
+            this.seats = seats;
+            this.carryingCapacity = carryingCapacity;
+            this.color = color;
+            this.movementSpeed = movementSpeed;
+            this.waterDrag = waterDrag;
+        }
+    }
+}
