95c30c9dde19bc173b9f4d64551e6c029e3de2e3
Jarryd Huntley <Jarryd Huntley>
2017-04-13 14:37:15 -0400

    minor refactoring.

--- Week3_day4-Spring17/Program.cs
+++ Week3_day4-Spring17/Program.cs
@@ -14,17 +14,17 @@ namespace Week3_day4_Spring17
             Automobile auto1 = new Automobile(4, 15, 4, 5, 2000, "Blue", 80.0d);
             auto1.Move();
             auto1.Move();
-            Console.WriteLine("Automobile Distance Traveled: " + auto1.DistanceTraveled + "\n");
+            Console.WriteLine("Automobile "+auto1.GetDistanceTraveled());
 
             Boat boat1 = new Boat(2, 500, "White", 80.0d, .90d);
             boat1.Move();
             boat1.Move();
-            Console.WriteLine("Boat Distance Traveled: " + boat1.DistanceTraveled + "\n");
+            Console.WriteLine("Boat "+boat1.GetDistanceTraveled());
 
             Aircraft aircraft1 = new Aircraft(70, 100000, "Grey", 600);
             aircraft1.Move();
             aircraft1.Move();
-            Console.WriteLine("Aircraft Distance Traveled: " + aircraft1.DistanceTraveled + "\n");
+            Console.WriteLine("Aircraft " + aircraft1.GetDistanceTraveled());
         }
     }
 }
