55e234c0cc19eb5194f8025b5f6d299e0a899096
Jarryd Huntley <Jarryd Huntley>
2017-04-13 10:10:08 -0400

    Added in the vehicle class and some fields.

---
+++ Week3_day4-Spring17/Vehicle.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week3_day4_Spring17
+{
+    class Vehicle
+    {
+        private int seats;
+        private int carryingCapacity;
+        private string color;
+        private double movementSpeed;
+        private float gearRatio;
+        private float engineSize;
+
+        public int Seats
+        {
+            get { return this.seats; }
+        }
+
+        public int CarryingCapacity
+        {
+            get { return this.carryingCapacity; }
+        }
+
+        public string Color
+        {
+            get { return this.color; }
+        }
+
+    }
+}
