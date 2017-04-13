0053c231723363fd1db8c9dad261f9a93f17b469
Jarryd Huntley <Jarryd Huntley>
2017-04-13 10:52:40 -0400

    Tested the vehicle class.

--- Week3_day4-Spring17/Program.cs
+++ Week3_day4-Spring17/Program.cs
@@ -10,6 +10,11 @@ namespace Week3_day4_Spring17
     {
         static void Main(string[] args)
         {
+            Vehicle vehicle1 = new Vehicle(4, 2000, "Green", 55.0d);
+            vehicle1.Move();
+            vehicle1.Move();
+
+            Console.WriteLine(vehicle1.DistanceTraveled);
         }
     }
 }
